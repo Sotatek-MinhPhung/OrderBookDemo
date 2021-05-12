fc9377083c8559806e64e280428b827853e95712
Phung Duc Minh <ducmin248@gmail.com>
2021-05-12 10:05:11 +0700

    generate store and demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react'
 
-function App() {
-    return (
-        <>
-        </>
-    );
-}
+const App = () => (
+    <>
+    </>
+);
 
 export default App;
---
+++ src/store/reducer/rootReducer.js
@@ -0,0 +1,7 @@
+import {combineReducers} from "redux";
+
+const rootReducer = combineReducers({
+    // todos: todoReducer
+})
+
+export default rootReducer
